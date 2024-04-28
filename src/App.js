aadad900f108f8c6f6465a0a7b7f1824645b9a5f
prashithak <135314888+prashithak@users.noreply.github.com>
2024-04-28 23:04:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Admin_dash from './components/Admin_dash';
+import Student from './components/Student';
 
 function App() {
   return (
     <div>
      <Routes>
         <Route path={'/'} element={<Home/>}></Route>
+       <Route path={'/addash'} element={<Admin_dash/>}></Route>
+        <Route path={'/list'}  element={<Student/>}></Route>
       </Routes>
     </div>
   );
