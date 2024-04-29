3a75fee07d420afbd37d12360446bf7e72b6f8e9
indu196619 <168345722+indu196619@users.noreply.github.com>
2024-04-29 12:05:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Admin_dash from './components/Admin_dash';
 import Student from './components/Student';
+import StudentDashboard from './components/Studentdashboard';
+import Login from './components/Login';
+import Main from './components/Main';
+
 
 function App() {
   return (
@@ -12,6 +16,11 @@ function App() {
         <Route path={'/'} element={<Home/>}></Route>
        <Route path={'/addash'} element={<Admin_dash/>}></Route>
         <Route path={'/list'}  element={<Student/>}></Route>
+        <Route path={'/s'} element={<Main child={<StudentDashboard/>}/>}></Route>
+       <Route path={'/l'} element={<Login/>}></Route>
+        <Route path={'/list'}  element={<Student/>}></Route>
+        
+    
       </Routes>
     </div>
   );
